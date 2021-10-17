f37b5adde6b3f69710ac211977cfd1fb1ed3ae63
Beet-le <1693332907@qq.com>
2021-10-17 23:19:45 +0800

    Day10.15

--- src/main/java/com/bt/servlet/SeasonDemo01.java
+++ src/main/java/com/bt/servlet/SeasonDemo01.java
@@ -4,6 +4,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class SeasonDemo01 extends HttpServlet {
@@ -12,6 +13,23 @@ public class SeasonDemo01 extends HttpServlet {
         req.setCharacterEncoding("utf-8");
         resp.setCharacterEncoding("utf-8");
         resp.setContentType("text/html;charset=utf-8");
+        //得到Session
+        HttpSession session = req.getSession();
+        //给Session中存东西
+        session.setAttribute("name","Beetle");
+        //获取Session的ID
+        String sessionId = session.getId();
+        //判断Session是不是新创建
+        if (session.isNew()){
+            resp.getWriter().write("session创建成功,ID:"+sessionId);
+        }else {
+            resp.getWriter().write("session以及在服务器中存在了,ID:"+sessionId);
+        }
+
+        //Session创建的时候做了什么事情；
+//        Cookie cookie = new Cookie("JSESSIONID",sessionId);
+//        resp.addCookie(cookie);
+
     }
 
     @Override
